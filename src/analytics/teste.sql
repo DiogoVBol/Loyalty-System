d74c2b6392f3b30f65cb256d42758fec013d64fd
diogovierib <diogovierib@gmail.com>
2025-11-14 22:24:50 -0300

    features platform

--- src/analytics/teste.sql
+++ src/analytics/teste.sql
@@ -1,11 +1,3 @@
-SELECT dtRef, descLifeCycle,count(*) AS qtdeClientes
+SELECT 
 
-
-
-FROM life_cycle
-
-WHERE descLifeCycle <> '05-ZUMBI'
-AND dtRef = (SELECT MAX(dtRef) from life_cycle)
-
-GROUP BY dtRef, descLifeCycle
 
